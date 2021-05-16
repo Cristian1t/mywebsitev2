aaab049d51307ffe807bcee4981b836b35235a99
Cristian <31905715+Cristian1t@users.noreply.github.com>
2021-05-16 23:20:56 +0200

    old site link

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -8,7 +8,7 @@ function Projects() {
         Check out my <a href="https://github.com/Cristian1t">github</a>.
       </h2>
       <h2>
-        Or my <a href="cristianturmacu.com">old website</a>.
+        Or my <a href="cristianturmacu-oldwebsite.netlify.app">old website</a>.
       </h2>
     </div>
   );
